bcf995dfdfa043c4a08d69f51e1c6973ce8aab56
Kovacevic Sam <114364206+kovasah@users.noreply.github.com>
2022-11-23 19:07:49 +0100

    final script version

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const msg = new SpeechSynthesisUtterance(document.querySelector('[name="text"]').textContent);
+const msg = new SpeechSynthesisUtterance();
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll(
@@ -7,13 +7,49 @@ const options = document.querySelectorAll(
 const speakButton = document.querySelector("#speak");
 const stopButton = document.querySelector("#stop");
 
-function speakNow() {
-    message.lang = "en-US";
-    
-    const voices = speechSynthesis.getVoices().filter(voice => voice.lang === "en-US");
-  message.voice = voices[0];
-  
-    speechSynthesis.speak(message);
-   }
+msg.text = document.querySelector('[name="text"]').value;
 
-speakButton.addEventListener("click", speakNow);
+// function speakNow() {
+//     message.lang = "en-US";
+    
+//     const voices = speechSynthesis.getVoices().filter(voice => voice.lang === "en-US");
+//   message.voice = voices[0];
+  
+//     speechSynthesis.speak(message);
+//    }
+
+// speakButton.addEventListener("click", speakNow);
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+   // .filter(voice => voice.lang.includes('en'))
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
+
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle();
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+
+options.forEach(option => option.addEventListener('change', setOption));
+
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
